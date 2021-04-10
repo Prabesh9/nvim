e71603df7fc0fc128828a8cdff0b02ae7aa2d75d
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-04-10 11:50:27 +0545

    nvim-tree

--- init.lua
+++ init.lua
@@ -1,14 +1,8 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
-end
-
+require('config.packer-auto')
 require('plugin')
-require('config.theme')
+require('theme')
 require('config.my_statusline')
+
 vim.cmd('source ~/.config/nvim/vim-script/nvim-tree.vim')
+vim.cmd('source ~/.config/nvim/general/settings.vim')
+vim.cmd('source ~/.config/nvim/keys/mappings.vim')
--- vim-plug/plugins.vim
+++
@@ -1,6 +0,0 @@
-call plug#begin()
-    Plug 'tomasiser/vim-code-dark'                      " vscode theme
- "  Plug 'glepnir/galaxyline.nvim' , {'branch': 'main'}
-    Plug 'kyazdani42/nvim-web-devicons' " for file icons
-    Plug 'kyazdani42/nvim-tree.lua'
-call plug#end()
