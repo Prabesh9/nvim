861752480481c639535ad5345d464747796b3b4e
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-06-11 15:20:33 +0545

    mappings converted from vim to lua

---
+++ lua/mappings.lua
@@ -0,0 +1,59 @@
+---@diagnostic disable: undefined-global
+-- better window movement
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
+
+-- TODO fix this
+-- Terminal window navigation
+vim.cmd([[
+  tnoremap <C-h> <C-\><C-N><C-w>h
+  tnoremap <C-j> <C-\><C-N><C-w>j
+  tnoremap <C-k> <C-\><C-N><C-w>k
+  tnoremap <C-l> <C-\><C-N><C-w>l
+  inoremap <C-h> <C-\><C-N><C-w>h
+  inoremap <C-j> <C-\><C-N><C-w>j
+  inoremap <C-k> <C-\><C-N><C-w>k
+  inoremap <C-l> <C-\><C-N><C-w>l
+  tnoremap <Esc> <C-\><C-n>
+]])
+
+-- TODO fix this
+-- resize with arrows
+vim.api.nvim_set_keymap('n', '<A-k>', ':resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<A-j>', ':resize +2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<A-h>', ':vertical resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<A-l>', ':vertical resize +2<CR>', {silent = true})
+
+-- better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+
+-- I hate escape
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+
+-- Tab switch buffer
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+
+-- Move selected line / block of text in visual mode
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- Better nav for omnicomplete
+vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+
+
+vim.cmd('vnoremap p "0p')
+vim.cmd('vnoremap P "0P')
+
+-- Alternate way to save
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', {noremap = true, silent = true})
+
+-- Alternate way to quit
+vim.api.nvim_set_keymap('n', '<C-Q>', ':q!<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-c>', ':bw<CR>', {noremap = false, silent = true})
