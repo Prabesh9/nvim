01b81e9fcc7cb452603b0e0ed51d633bc0510325
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-06-11 15:05:01 +0545

    indent lines '|' removed from dashboard

--- lua/lc-dashboard/init.lua
+++ lua/lc-dashboard/init.lua
@@ -1,7 +1,7 @@
+---@diagnostic disable: undefined-global
 local vg= vim.g;
 
 vg.dashboard_custom_header={
-'',
 '',
 '',
 '',
@@ -19,3 +19,4 @@ vg.dashboard_custom_header={
 
 vg.dashboard_default_executive='telescope'
 vg.dashboard_custom_footer={'prabesh maharjan'}
+vg.indentLine_fileTypeExclude={'dashboard'}
